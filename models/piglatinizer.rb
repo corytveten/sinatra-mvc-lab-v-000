5a95923b4e02a8ac933b29882e36e07809575a52
Cory Tveten <corytveten@gmail.com>
2018-09-09 18:16:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,13 +7,13 @@ class PigLatinizer
     consonants = alpha - vowels
 
     if vowels.include?(str[0])
-      str + 'ay'
+      str + 'way'
     elsif consonants.include?(str[0]) && consonants.include?(str[1])
       str[2..-1] + str[0..1] + 'ay'
     elsif consonants.include?(str[0])
       str[1..-1] + str[0] + 'ay'
     else
-      str + 'ay'# return unchanged
+      str + 'way'# return unchanged
     end
   end
 end
