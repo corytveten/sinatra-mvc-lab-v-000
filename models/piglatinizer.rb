069b0d4bef9a7e008f53e446597c8a5734e69299
Cory Tveten <corytveten@gmail.com>
2018-09-09 18:19:13 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,22 +7,22 @@ class PigLatinizer
   vowel = str1.scan(/\w/)
   alpha =('a'..'z').to_a
   con = (alpha - vowel).join
-  word = str.scan(/\w/)  
-  if  #first rule 
+  word = str.scan(/\w/)
+  if  #first rule
     str =~ str2
-    str + "ay" 
-  elsif # second rule 
-    str != str2 
+    str + "ay"
+  elsif # second rule
+    str != str2
     s = str.slice!(/^./)
     str + s + "ay"
-  elsif 
+  elsif
     word[0.1]=~(/\A[con]/)
     s = str.slice!(/^../)
     str + s + "ay"
   else
-    word[0..2]=~(/\A[con]/) 
+    word[0..2]=~(/\A[con]/)
     s = str.slice!(/^.../)
-    str + s + "ay" 
+    str + s + "ay"
   end
 end
 end
