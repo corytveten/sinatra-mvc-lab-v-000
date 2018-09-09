b5a7e8c7fed986d69aac02f52907c67385dbcf00
Cory Tveten <corytveten@gmail.com>
2018-09-09 18:19:09 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,28 @@
 
 class PigLatinizer
 
-  def piglatinize str
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alpha - vowels
-
-    if vowels.include?(str[0])
-      str + 'way'
-    elsif consonants.include?(str[0]) && consonants.include?(str[1])
-      str[2..-1] + str[0..1] + 'ay'
-    elsif consonants.include?(str[0])
-      str[1..-1] + str[0] + 'ay'
-    else
-      str # return unchanged
-    end
+  def piglatinize (str)
+  str1="aeiou"
+  str2=(/\A[aeiou]/)
+  vowel = str1.scan(/\w/)
+  alpha =('a'..'z').to_a
+  con = (alpha - vowel).join
+  word = str.scan(/\w/)  
+  if  #first rule 
+    str =~ str2
+    str + "ay" 
+  elsif # second rule 
+    str != str2 
+    s = str.slice!(/^./)
+    str + s + "ay"
+  elsif 
+    word[0.1]=~(/\A[con]/)
+    s = str.slice!(/^../)
+    str + s + "ay"
+  else
+    word[0..2]=~(/\A[con]/) 
+    s = str.slice!(/^.../)
+    str + s + "ay" 
   end
+end
 end
