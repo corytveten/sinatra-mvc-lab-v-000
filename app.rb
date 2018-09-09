984c559e3064865d4c33782abb9383acffbed6f6
Cory Tveten <corytveten@gmail.com>
2018-09-09 18:31:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @pig_latin = PigLatinizer.new(params[:user_input]
+    @pig_latin = PigLatinizer.new(params[:user_phrase]
     erb :results
   end
 
